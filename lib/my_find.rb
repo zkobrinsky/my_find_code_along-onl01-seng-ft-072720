447bad90fe1a6457bfb5e7288c0af3b963672c7a
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-30 19:37:33 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,10 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-    return (collection[i]) if yield(collection[i])
+    if yield(collection[i])
+      puts collection[i]
+      return collection[i]
+    # return (collection[i]) if yield(collection[i])
     i = i+1
   end
 end
